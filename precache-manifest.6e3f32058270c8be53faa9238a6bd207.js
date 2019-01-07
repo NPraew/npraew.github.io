a6afee77ec82a3b23a2712705b6c90ad570c6820
Praewpipat, Nattapol (Agoda) <nattapol.praewpipat@agoda.com>
2019-01-07 17:24:55 +0700

    Updates

---
+++ precache-manifest.6e3f32058270c8be53faa9238a6bd207.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2602e451eb26b6f04c1c",
+    "url": "/static/css/main.a847538b.chunk.css"
+  },
+  {
+    "revision": "2602e451eb26b6f04c1c",
+    "url": "/static/js/main.2602e451.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a473e679448ecf0559b678bd627f2d03",
+    "url": "/index.html"
+  }
+];
